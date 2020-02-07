75fbdd70078e006de96a13593e84a1b9d3e9d65d
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-07 21:40:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def batch_badge_creator(names)
+  names.each do |name|
+    puts "Hello, my name is #{name}"
+  end
+end
+
+def assign_rooms(names)
+  names.each_with_index {|name, index| 
+    puts "Hello, #{name}! You'll be assigned to room #{index}!"}
+end
+  
